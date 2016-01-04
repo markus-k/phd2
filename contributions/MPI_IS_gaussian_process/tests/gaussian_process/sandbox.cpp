07740e1d9045a87ea9dd71d19a2f2d98915040fe
Edgar Klenske <edgar.klenske@tuebingen.mpg.de>
2016-01-04 16:41:31 +0100

    Tidied up the covariance functions and removed the Dirac delta covariance function.
    - include only the two needed ones
    - Source reformatting and CLRF setting
    - Fixed a bug in CMakeLists.txt

--- contributions/MPI_IS_gaussian_process/tests/gaussian_process/sandbox.cpp
+++ contributions/MPI_IS_gaussian_process/tests/gaussian_process/sandbox.cpp
@@ -45,152 +45,166 @@
 #include <ctime>
 #include <string>
 
-int main(int argc, char **argv)
+int main(int argc, char** argv)
 {
 
-  ///// Play with the spectrum /////
-  double Tmax = 1024;
-  int res = 1024;
+    ///// Play with the spectrum /////
+    double Tmax = 1024;
+    int res = 1024;
 
-  double p = 512;
-  double s2 = 200;
-  double sigma = 100;
+    double p = 512;
+    double s2 = 200;
+    double sigma = 100;
 
-  Eigen::VectorXd X = Eigen::VectorXd::LinSpaced(res,0,Tmax);
-  Eigen::VectorXd Y = s2*(2*M_PI*X/p).array().sin();
+    Eigen::VectorXd X = Eigen::VectorXd::LinSpaced(res, 0, Tmax);
+    Eigen::VectorXd Y = s2 * (2 * M_PI * X / p).array().sin();
 
-  int N_fft = 4096;
+    int N_fft = 4096;
 
-  Eigen::ArrayXd W = math_tools::hamming_window(Y.rows());
+    Eigen::ArrayXd W = math_tools::hamming_window(Y.rows());
 
-  Eigen::VectorXd Yw = Y.array()*W;
+    Eigen::VectorXd Yw = Y.array() * W;
 
-  clock_t begin = std::clock();
-  std::pair<Eigen::VectorXd, Eigen::VectorXd> result = math_tools::compute_spectrum(Yw, N_fft);
-  clock_t end = std::clock();
-  double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
-  std::cout << "time for FFT: " << elapsed_secs << " s." << std::endl;
+    clock_t begin = std::clock();
+    std::pair<Eigen::VectorXd, Eigen::VectorXd> result = math_tools::compute_spectrum(Yw, N_fft);
+    clock_t end = std::clock();
+    double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
+    std::cout << "time for FFT: " << elapsed_secs << " s." << std::endl;
 
 
-  Eigen::VectorXd amplitudes = result.first;
-  Eigen::VectorXd frequencies = result.second;
+    Eigen::VectorXd amplitudes = result.first;
+    Eigen::VectorXd frequencies = result.second;
 
-  double dt = Tmax/res;
-  frequencies /= dt;
+    double dt = Tmax / res;
+    frequencies /= dt;
 
-  Eigen::VectorXd periods = 1/frequencies.array();
+    Eigen::VectorXd periods = 1 / frequencies.array();
 
-  assert(amplitudes.size() == frequencies.size());
+    assert(amplitudes.size() == frequencies.size());
 
-  Eigen::VectorXd::Index maxIndex;
-  amplitudes.maxCoeff(&maxIndex);
+    Eigen::VectorXd::Index maxIndex;
+    amplitudes.maxCoeff(&maxIndex);
 
-  std::cout << maxIndex << std::endl;
-  std::cout << periods(maxIndex) << std::endl;
+    std::cout << maxIndex << std::endl;
+    std::cout << periods(maxIndex) << std::endl;
 
-  std::ofstream outfile;
-  outfile.open("spectrum_data.csv", std::ios_base::out);
-  if(outfile.is_open()) {
-    outfile << "frequency, amplitude\n";
-    for( int i = 0; i < amplitudes.size(); ++i) {
-      outfile << std::setw(8) << frequencies[i] << "," << std::setw(8) << amplitudes[i] << "\n";
+    std::ofstream outfile;
+    outfile.open("spectrum_data.csv", std::ios_base::out);
+    if (outfile.is_open())
+    {
+        outfile << "frequency, amplitude\n";
+        for (int i = 0; i < amplitudes.size(); ++i)
+        {
+            outfile << std::setw(8) << frequencies[i] << "," << std::setw(8) << amplitudes[i] << "\n";
+        }
     }
-  } else {
-    std::cout << "unable to write to file" << std::endl;
-  }
-  outfile.close();
-
-  ///// Play with the GP /////
-
-  // first, read data from file
-  double buff[static_cast<int>(1e6)];
-  int rows=0;
-  int cols=0;
-
-  // Read numbers from file into buffer.
-  std::ifstream infile;
-  infile.open("gear_error.csv");
-
-  while (! infile.eof())
-  {
-    std::string line;
-    std::getline(infile, line);
-
-    int temp_cols = 0;
-    std::stringstream stream(line);
-    std::string token;
-
-    while(std::getline(stream, token, ',')) {
-      std::stringstream temp(token);
-      temp >> buff[cols*rows+temp_cols++];
+    else
+    {
+        std::cout << "unable to write to file" << std::endl;
     }
+    outfile.close();
 
-    if (temp_cols == 0)
-      continue;
+    ///// Play with the GP /////
 
-    if (cols == 0)
-      cols = temp_cols;
+    // first, read data from file
+    double buff[static_cast<int>(1e6)];
+    int rows = 0;
+    int cols = 0;
 
-    rows++;
-  }
-  infile.close();
-  rows--;
+    // Read numbers from file into buffer.
+    std::ifstream infile;
+    infile.open("gear_error.csv");
 
-  // Populate matrix with numbers.
-  Eigen::MatrixXd data(rows,cols);
-  for (int i = 0; i < rows; i++)
-    for (int j = 0; j < cols; j++)
-      data(i,j) = buff[ cols*i+j ];
+    while (! infile.eof())
+    {
+        std::string line;
+        std::getline(infile, line);
 
-  // prepare the data from this matrix
-  Eigen::VectorXd time = data.col(1);
-  Eigen::VectorXd ra_raw_dist = data.col(5);
-  Eigen::VectorXd control = data.col(7);
-  Eigen::VectorXd meas(time.rows());
+        int temp_cols = 0;
+        std::stringstream stream(line);
+        std::string token;
 
-  meas(0) = control(0);
-  for (int i = 1; i<time.rows(); ++i) {
-    meas(i) = meas(i-1) + control(i);
-  }
-  meas += ra_raw_dist;
+        while (std::getline(stream, token, ','))
+        {
+            std::stringstream temp(token);
+            temp >> buff[cols * rows + temp_cols++];
+        }
 
-  Eigen::VectorXd hyper_parameters(7);
-  hyper_parameters << 10, 1, 0.5, 500, 10, 500, 1;
+        if (temp_cols == 0)
+            continue;
 
-  covariance_functions::PeriodicSquareExponential2 covariance_function(hyper_parameters.array().log());
-  GP gp(1e0, covariance_function);
+        if (cols == 0)
+            cols = temp_cols;
 
-  gp.enableExplicitTrend();
-  gp.infer(time, meas);
-
-  int M = 512; // number of prediction points
-  Eigen::VectorXd locations = Eigen::VectorXd::LinSpaced(M, 0, time.maxCoeff() + 1500);
-  std::pair<Eigen::VectorXd, Eigen::MatrixXd> predictions = gp.predict(locations);
-
-  Eigen::VectorXd means = predictions.first;
-  Eigen::VectorXd stds = predictions.second.diagonal();
-
-  outfile.open("measurement_data.csv", std::ios_base::out);
-  if(outfile.is_open()) {
-    outfile << "location, output\n";
-    for( int i = 0; i < time.size(); ++i) {
-      outfile << std::setw(8) << time[i] << "," << std::setw(8) << meas[i] << "\n";
+        rows++;
     }
-  } else {
-    std::cout << "unable to write to file" << std::endl;
-  }
-  outfile.close();
+    infile.close();
+    rows--;
 
-  outfile.open("gp_data.csv", std::ios_base::out);
-  if(outfile.is_open()) {
-    outfile << "location, mean, std\n";
-    for( int i = 0; i < locations.size(); ++i) {
-      outfile << std::setw(8) << locations[i] << "," << std::setw(8) << means[i] << "," << std::setw(8) << stds[i] << "\n";
+    // Populate matrix with numbers.
+    Eigen::MatrixXd data(rows, cols);
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < cols; j++)
+            data(i, j) = buff[ cols * i + j ];
+
+    // prepare the data from this matrix
+    Eigen::VectorXd time = data.col(1);
+    Eigen::VectorXd ra_raw_dist = data.col(5);
+    Eigen::VectorXd control = data.col(7);
+    Eigen::VectorXd meas(time.rows());
+
+    meas(0) = control(0);
+    for (int i = 1; i < time.rows(); ++i)
+    {
+        meas(i) = meas(i - 1) + control(i);
     }
-  } else {
-    std::cout << "unable to write to file" << std::endl;
-  }
-  outfile.close();
+    meas += ra_raw_dist;
+
+    Eigen::VectorXd hyper_parameters(7);
+    hyper_parameters << 10, 1, 0.5, 500, 10, 500, 1;
+
+    covariance_functions::PeriodicSquareExponential2 covariance_function(hyper_parameters.array().log());
+    GP gp(1e0, covariance_function);
+
+    gp.enableExplicitTrend();
+    gp.infer(time, meas);
+
+    int M = 512; // number of prediction points
+    Eigen::VectorXd locations = Eigen::VectorXd::LinSpaced(M, 0, time.maxCoeff() + 1500);
+    std::pair<Eigen::VectorXd, Eigen::MatrixXd> predictions = gp.predict(locations);
+
+    Eigen::VectorXd means = predictions.first;
+    Eigen::VectorXd stds = predictions.second.diagonal();
+
+    outfile.open("measurement_data.csv", std::ios_base::out);
+    if (outfile.is_open())
+    {
+        outfile << "location, output\n";
+        for (int i = 0; i < time.size(); ++i)
+        {
+            outfile << std::setw(8) << time[i] << "," << std::setw(8) << meas[i] << "\n";
+        }
+    }
+    else
+    {
+        std::cout << "unable to write to file" << std::endl;
+    }
+    outfile.close();
+
+    outfile.open("gp_data.csv", std::ios_base::out);
+    if (outfile.is_open())
+    {
+        outfile << "location, mean, std\n";
+        for (int i = 0; i < locations.size(); ++i)
+        {
+            outfile << std::setw(8) << locations[i] << "," << std::setw(8) << means[i] << "," << std::setw(8) << stds[i] << "\n";
+        }
+    }
+    else
+    {
+        std::cout << "unable to write to file" << std::endl;
+    }
+    outfile.close();
 
 }
 
